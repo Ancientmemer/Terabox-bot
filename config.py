b929609667a781d1f20879dd0758914dc9e1029a
Ancientmemer <ancientmemer12@gmail.com>
2025-12-24 13:01:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
-API_ID = 123456
-API_HASH = "your_api_hash"
-BOT_TOKEN = "your_bot_token"
+import os
 
-OWNER_ID = 123456789  # your telegram id
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+OWNER_ID = int(os.environ.get("OWNER_ID"))
